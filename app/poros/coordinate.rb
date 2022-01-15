204b8d641fbc8bdcbb4558f8969c485debd6f3a8
Nathan Brown <nathan.brown263@gmail.com>
2022-01-15 12:02:52 -0700

    testing: Add testing for Coordinate poro

--- app/poros/coordinate.rb
+++ app/poros/coordinate.rb
@@ -1,4 +1,7 @@
 class Coordinate
+  attr_reader :latitude,
+              :longitude
+              
   def initialize(data)
     @latitude = data[:lat]
     @longitude = data[:lng]
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,3 +62,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+def coordinate_data
+  {:lat=>39.738453, :lng=>-104.984853}
+end
