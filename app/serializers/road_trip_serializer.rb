f63ec3f2731e1e70afca50ebb07a728c3d5d9072
Nathan Brown <nathan.brown263@gmail.com>
2022-01-16 19:46:45 -0700

    testing: Add tests for Road Trip API

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,7 +1,6 @@
 class RoadTripSerializer
     include JSONAPI::Serializer
-  attributes :id,
-             :start_city,
+  attributes :start_city,
              :end_city,
              :travel_time,
              :weather_at_eta
--- spec/requests/api/v1/road_trip_requests_spec.rb
+++ spec/requests/api/v1/road_trip_requests_spec.rb
@@ -20,6 +20,21 @@ describe 'RoadTrip API' do
       road_trip = JSON.parse(response.body, symbolize_names: true)
 
       expect(road_trip).to have_key(:data)
+      expect(road_trip[:data]).to have_key(:id)
+
+      expect(road_trip[:data][:type]).to eq("road_trip")
+      expect(road_trip[:data]).to have_key(:attributes)
+      expect(road_trip[:data][:attributes]).to have_key(:start_city)
+      expect(road_trip[:data][:attributes][:start_city]).to be_a String
+
+      expect(road_trip[:data][:attributes]).to have_key(:end_city)
+      expect(road_trip[:data][:attributes][:end_city]).to be_a String
+
+      expect(road_trip[:data][:attributes]).to have_key(:travel_time)
+      expect(road_trip[:data][:attributes][:travel_time]).to be_a String
+
+      expect(road_trip[:data][:attributes]).to have_key(:weather_at_eta)
+      expect(road_trip[:data][:attributes][:weather_at_eta]).to be_a Hash
     end
   it 'works with trips longer than 8 hours', :vcr do
     params = {
@@ -34,5 +49,20 @@ describe 'RoadTrip API' do
     road_trip = JSON.parse(response.body, symbolize_names: true)
 
     expect(road_trip).to have_key(:data)
+    expect(road_trip[:data]).to have_key(:id)
+
+    expect(road_trip[:data][:type]).to eq("road_trip")
+    expect(road_trip[:data]).to have_key(:attributes)
+    expect(road_trip[:data][:attributes]).to have_key(:start_city)
+    expect(road_trip[:data][:attributes][:start_city]).to be_a String
+
+    expect(road_trip[:data][:attributes]).to have_key(:end_city)
+    expect(road_trip[:data][:attributes][:end_city]).to be_a String
+
+    expect(road_trip[:data][:attributes]).to have_key(:travel_time)
+    expect(road_trip[:data][:attributes][:travel_time]).to be_a String
+
+    expect(road_trip[:data][:attributes]).to have_key(:weather_at_eta)
+    expect(road_trip[:data][:attributes][:weather_at_eta]).to be_a Hash
   end
 end
